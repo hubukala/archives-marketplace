0c3f3710f98ff595630931b205f89aa69bcdf7a5
Hubert Bukala <hubertbukala@icloud.com>
2022-08-30 17:51:34 +0200

    added primary button

---
+++ src/styles/ButtonPrimary.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const ButtonPrimary = styled.button`
+  margin-left: auto;
+  margin-right: 0;
+  height: 34px;
+  font-size: 12px;
+  font-weight: bold;
+  border: 1px solid #c5c5c5;
+  background-color: white;
+  transition: all 0.3s;
+  &:hover {
+      background-color: #e7e7e7;
+      border: 1px solid black;
+  }
+`
+
+export { ButtonPrimary };
