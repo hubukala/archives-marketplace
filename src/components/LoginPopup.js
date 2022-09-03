8bf42b30af59ec2fdc31326b1dece7758191a426
Hubert Bukala <hubertbukala@icloud.com>
2022-09-03 19:47:10 +0200

    added toggle feature

--- src/components/LoginPopup.js
+++ src/components/LoginPopup.js
@@ -1,17 +1,40 @@
 import styled from "styled-components";
-import { SearchInput } from "../styles/SearchBarStyles";
 import { ButtonSecondary } from "../styles/ButtonSecondary";
+
+const BackgroundContainer = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.75);
+`
 
 const LoginContainer = styled.div`
     background-color: #e8e8e8;
-    border: 1px solid black;
     border-radius: 15px;
-    display: block;
-    margin: auto;
-    width: 40%;
-    max-width: 400px;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 400px;
+    top: 50%;
+    left: 50%;
+    margin-top: -200px;
+    margin-left: -200px;
     min-width: 280px;
 `
+
+const LoginWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 const LoginForm = styled.form`
     display: block;
@@ -38,22 +61,28 @@ const Paragraph = styled.p`
     border-bottom: 1px solid black;
 `
 
-const LoginPopup = ({ isShowLogin }) => {
+const LoginPopup = ({ showLogin, setShowLogin }) => {
     return (
         <div>
-            <LoginContainer>
-                <LoginForm action="">
-                    <h1>Sign in</h1>
-                    <Paragraph>To post your item for sale or make a purchase</Paragraph>
-                    <LoginLabel htmlFor="">Username</LoginLabel> <br />
-                    <LoginInput type="text" name="username" /> <br />
-                    <LoginLabel htmlFor="">Password</LoginLabel> <br />
-                    <LoginInput type="text" name="password" /> <br />
-                    <ButtonSecondary>LOGIN</ButtonSecondary>
-                </LoginForm>
-            </LoginContainer>
+            { showLogin ?
+                <BackgroundContainer>
+                    <LoginContainer>
+                        <LoginWrapper>
+                            <LoginForm action="">
+                                <h1>Sign in</h1>
+                                <Paragraph>To post your item for sale or make a purchase</Paragraph>
+                                <LoginLabel htmlFor="">Username</LoginLabel> <br />
+                                <LoginInput type="text" name="username" /> <br />
+                                <LoginLabel htmlFor="">Password</LoginLabel> <br />
+                                <LoginInput type="text" name="password" /> <br />
+                                <ButtonSecondary>LOGIN</ButtonSecondary>
+                            </LoginForm>
+                        </LoginWrapper>
+                    </LoginContainer>
+                </BackgroundContainer>
+            : null }
         </div>
-    )
+    );
 }
 
 export default LoginPopup;
