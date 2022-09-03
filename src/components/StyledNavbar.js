d3bc3ec14359f3668f476d45fc5b21c7c6f6b707
Hubert Bukala <hubertbukala@icloud.com>
2022-09-03 19:50:45 +0200

    added logic for login popup

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -1,37 +1,46 @@
 import { Navbar } from "../styles/Navbar";
 import { Links } from "../styles/Links";
 import SearchBar from "./SearchBar";
+import { useState } from "react";
+import LoginPopup from "./LoginPopup";
+import { ButtonPrimary } from "../styles/ButtonPrimary";
+import { ButtonSecondary } from "../styles/ButtonSecondary";
 
 const StyledNavbar = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const openLogin = () => {
+    setShowLogin(prev => !prev);
+  }
+
   return (
-    <Navbar>
-      <Links
-        to="/"
-      >
-        ARCHIVES MARKETPLACE
-      </Links>
-      <SearchBar />
-      <Links
-        to="/sell"
-      >
-        SELL
-      </Links>
-      <Links
-        to="/shop"
-      >
-        SHOP
-      </Links>
-      <Links
-        to="/login"
-      >
-        LOGIN
-      </Links>
-      <Links
-        to="/signup"
-      >
-        SIGN UP
-      </Links>
-    </Navbar>
+    <div>
+      <Navbar>
+        <Links
+          to="/"
+        >
+          ARCHIVES MARKETPLACE
+        </Links>
+        <SearchBar />
+        <Links
+          to="/sell"
+        >
+          SELL
+        </Links>
+        <Links
+          to="/shop"
+        >
+          SHOP
+        </Links>
+        <ButtonPrimary onClick={openLogin}>
+          LOGIN
+        </ButtonPrimary>
+        <ButtonSecondary>
+          SIGN UP
+        </ButtonSecondary>
+      </Navbar>
+      <LoginPopup showLogin={showLogin} setShowLogin={setShowLogin}/>
+    </div>
   )
 };
 
