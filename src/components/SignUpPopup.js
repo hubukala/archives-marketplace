aaf2962dd8b7ba36b11393fcb6bbcfcffa29d709
Hubert Bukala <hubertbukala@icloud.com>
2022-09-04 13:57:52 +0200

    firebase authentication added

--- src/components/SignUpPopup.js
+++ src/components/SignUpPopup.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ButtonSecondary } from "../styles/ButtonSecondary";
 import { ButtonPrimary } from "../styles/ButtonPrimary";
 import { BackgroundContainer } from "../styles/Login-Signup/BackgroundContainer";
@@ -7,24 +8,53 @@ import { LoginForm } from "../styles/Login-Signup/LoginForm";
 import { LoginLabel } from "../styles/Login-Signup/LoginLabel";
 import { LoginInput } from "../styles/Login-Signup/LoginInput";
 import { Description } from "../styles/Login-Signup/Description";
+import { app } from "../firebaseConfig";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUpPopup = ({ showSignUp, setShowSignUp }) => {
+    let auth = getAuth();
+    const [data, setData] = useState({});
+
+    const handleInputs = (event) => {
+        let inputs = { [event.target.name]: event.target.value }
+
+        setData({ ...data, ...inputs })
+    };
+
+    const submitData = () => {
+        createUserWithEmailAndPassword(auth, data.email, data.password)
+            .then((response) => {
+                console.log(response.user)
+                setShowSignUp (prev => !prev)
+            })
+            .catch((err) => {
+                alert(err.message)
+            });
+    };
     return (
         <div>
             { showSignUp ?
                 <BackgroundContainer>
                     <LoginContainer>
                         <LoginWrapper>
-                            <LoginForm action="">
+                            <LoginForm action="javascript:void(0);">
                                 <h1>Sign up</h1>
                                 <Description>Create account to get full access to buying and selling.</Description>
                                 <LoginLabel htmlFor="">E-mail adress (Username)</LoginLabel> <br />
-                                <LoginInput type="text" name="username" /> <br />
+                                <LoginInput 
+                                    type="email" 
+                                    name="email"
+                                    onChange={event => handleInputs(event)}
+                                /> <br />
                                 <LoginLabel htmlFor="">Password</LoginLabel> <br />
-                                <LoginInput type="text" name="password" /> <br />
-                                <LoginLabel htmlFor="">Confirm Password</LoginLabel> <br />
-                                <LoginInput type="text" name="password" /> <br />
-                                <ButtonSecondary>SIGN UP</ButtonSecondary>
+                                <LoginInput 
+                                    type="password" 
+                                    name="password"
+                                    onChange={event => handleInputs(event)}
+                                /> <br />
+                                {/* <LoginLabel htmlFor="">Confirm Password</LoginLabel> <br />
+                                <LoginInput type="text" name="password" /> <br /> */}
+                                <ButtonSecondary onClick={submitData}>SIGN UP</ButtonSecondary>
                                 <ButtonPrimary onClick={() => setShowSignUp (prev => !prev)}>CLOSE</ButtonPrimary>
                             </LoginForm>
                         </LoginWrapper>
