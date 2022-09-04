db99a1b2c9fdecdc2fb1f7a530a7d0a902b77553
Hubert Bukala <hubertbukala@icloud.com>
2022-09-04 13:56:24 +0200

    created firebase project

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHB6VZErTU-D6y_BCN84oGAjdyCu2NH9s",
+  authDomain: "archives-marketplace.firebaseapp.com",
+  projectId: "archives-marketplace",
+  storageBucket: "archives-marketplace.appspot.com",
+  messagingSenderId: "654454385987",
+  appId: "1:654454385987:web:9709d9e39d60d84e9093f6",
+  measurementId: "G-SXG1EQP25Z"
+};
+
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
