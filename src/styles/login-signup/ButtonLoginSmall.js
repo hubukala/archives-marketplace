58193d9aa42c75efc09ab7c87fb6371a61be4782
Hubert Bukala <hubertbukala@icloud.com>
2022-09-10 20:06:50 +0200

    created login button for signup modal

---
+++ src/styles/login-signup/ButtonLoginSmall.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const ButtonLoginSmall = styled.button`
+    border: none;
+    background: none;
+    color: black;
+    font-size: 16px;
+    font-family: 'Helvetica';
+    transition: all 0.2s;
+    text-decoration: underline;
+    &:hover {
+        color:blue;
+    }
+`
+
+export { ButtonLoginSmall };
