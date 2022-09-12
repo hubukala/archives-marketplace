d6ad375353b609dcafd76ca9154615aa0fab0e61
Hubert Bukala <hubertbukala@icloud.com>
2022-09-12 17:22:43 +0200

    added login state

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,15 +1,18 @@
-import { Navigate, Outlet, useLocation  } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ setShowSignUp }) => {
     const auth = getAuth();
-    const location = useLocation()
     console.log(auth.currentUser);
-    
+    const toggle = () => {
+        setShowSignUp(true)
+    };
+
     if (auth.currentUser === null) {
-        return (<Navigate to="/" />)
+        toggle();
+        return (<Navigate to ="/" />);
     } else {
-        return (<Outlet />)
+        return (<Outlet />);
     }
 };
 
