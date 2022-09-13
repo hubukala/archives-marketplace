fd68c819201e9e43cf0f4b61cf1526a6b99156ce
Hubert Bukala <hubertbukala@icloud.com>
2022-09-13 19:48:00 +0200

    bugs fixed

--- src/components/AccountDetailsForm.js
+++ src/components/AccountDetailsForm.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { useState } from "react";
 import { ButtonSecondary } from "../styles/shared/buttons/ButtonSecondary";
 import { DetailsTextArea } from "../styles/account-details/DetailsTextArea";
 import { DetailsInput } from "../styles/account-details/DetailsInput";
@@ -6,48 +6,124 @@ import { DetailsLabel } from "../styles/account-details/DetailsLabel.js";
 import { DetailsSection } from "../styles/account-details/DetailsSection";
 import { ButtonContainer } from "../styles/account-details/ButtonContainer";
 import { DetailsContainer } from "../styles/account-details/DetailsContainer";
+import { db } from "../firebaseConfig";
+import { collection, addDoc, setDoc, doc, getDocs } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const AccountDetailsForm = () => {
+    const auth = getAuth()
+
+    const [data, setData] = useState({
+        fname: "",
+        lname: "",
+        bio: "",
+        city: "",
+        zipcode: "",
+        street: "",
+        suite: ""
+    })
+
+    const handleAdd = async (e) => {
+        e.preventDefault();
+        try {
+            await setDoc(doc(db, "users", auth.currentUser.uid), {
+                ...data,
+            });
+            setData({
+                fname: "",
+                lname: "",
+                bio: "",
+                city: "",
+                zipcode: "",
+                street: "",
+                suite: ""
+            });
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleInputs = (event) => {
+        let inputs = { [event.target.name]: event.target.value }
+        
+        setData({ ...data, ...inputs })
+    };
+
     return(          
         <form action="">
             <DetailsSection>
                 <DetailsContainer>
-                    <DetailsLabel for="fname">First name</DetailsLabel>
-                    <DetailsInput name="fname" type="text" />
+                    <DetailsLabel>First name</DetailsLabel>
+                    <DetailsInput 
+                        name="fname"
+                        type="text"
+                        onChange={handleInputs}
+                        value={data.fname}
+                    />
                 </DetailsContainer>
                 <DetailsContainer>
                     <DetailsLabel>Last name</DetailsLabel>
-                    <DetailsInput type="text" />
+                    <DetailsInput 
+                        name="lname"
+                        type="text"
+                        onChange={handleInputs}
+                        value={data.lname}
+                    />
                 </DetailsContainer>
             </DetailsSection>
             <DetailsSection>
                 <DetailsContainer>
                     <DetailsLabel>Bio</DetailsLabel>
-                    <DetailsTextArea type="text" />
+                    <DetailsTextArea 
+                        name="bio"
+                        type="text"
+                        onChange={handleInputs}
+                        value={data.bio}
+                    />
                 </DetailsContainer>
             </DetailsSection>
             <DetailsSection>
                 <DetailsContainer>
-                    <DetailsLabel for="city">City</DetailsLabel>
-                    <DetailsInput name="city" type="text" />
+                    <DetailsLabel>City</DetailsLabel>
+                    <DetailsInput 
+                        name="city"
+                        type="text"
+                        onChange={handleInputs}
+                        value={data.city}
+                    />
                 </DetailsContainer>
                 <DetailsContainer>
-                    <DetailsLabel for="zipcode">ZIP code</DetailsLabel>
-                    <DetailsInput name="zipcode" type="text" />
+                    <DetailsLabel>ZIP code</DetailsLabel>
+                    <DetailsInput 
+                        name="zipcode"
+                        type="text"
+                        onChange={handleInputs}
+                        value={data.zipcode}
+                    />
                 </DetailsContainer>
             </DetailsSection>
             <DetailsSection>
                 <DetailsContainer>
-                    <DetailsLabel for="zipcode">Street adress</DetailsLabel>
-                    <DetailsInput name="zipcode" type="text" />
+                    <DetailsLabel>Street adress</DetailsLabel>
+                    <DetailsInput 
+                        name="street" 
+                        type="text" 
+                        onChange={handleInputs}
+                        value={data.street}
+                    />
                 </DetailsContainer>
                 <DetailsContainer>
-                    <DetailsLabel for="suite">Apt/Suite</DetailsLabel>
-                    <DetailsInput name="suite" type="text" />
+                    <DetailsLabel>Apt/Suite</DetailsLabel>
+                    <DetailsInput 
+                        name="suite" 
+                        type="text" 
+                        onChange={handleInputs}
+                        value={data.suite}
+                    />
                 </DetailsContainer>
             </DetailsSection>
             <ButtonContainer>
-                <ButtonSecondary>SAVE</ButtonSecondary>
+                <ButtonSecondary type="submit" onClick={handleAdd}>SAVE</ButtonSecondary>
             </ButtonContainer>
         </form>
     )
