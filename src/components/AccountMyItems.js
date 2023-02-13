6c43aba5a127109e2e163d132e99487e9e0328e5
hubukala <hubertbukala@icloud.com>
2023-02-13 21:58:07 +0100

    added fetching items on 'my items' page

--- src/components/AccountMyItems.js
+++ src/components/AccountMyItems.js
@@ -1,6 +1,32 @@
+import { auth, db } from "../firebaseConfig"
+import { collection, query, where, getDocs } from "firebase/firestore"
+import { useEffect, useState } from "react"
+
 const AccountMyItems = () => {
+    const [data, setData] = useState({})
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const userId = auth.currentUser.uid
+            const q = query(collection(db, "products"), where("user_id", "==", userId))
+            const querySnapshot = await getDocs(q)
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data())
+                console.log(doc.data().price)
+                setData({
+                    "price": doc.data().price, 
+                    "category": doc.data().category
+                })
+            })
+        }
+        fetchData()
+    },[])
     return (
-        <h1>My Items</h1>
+        <>        
+            <h1>My Items</h1>
+            <h1>{data.price}</h1>
+            <h1>{data.category}</h1>
+        </>
     )
 }
 
