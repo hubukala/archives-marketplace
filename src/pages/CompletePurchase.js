ea112f06d78feda20c9bdb5cd9c1dad6d359a11c
hubukala <hubertbukala@icloud.com>
2023-02-25 15:18:45 +0100

    added complete purchase page

---
+++ src/pages/CompletePurchase.js
@@ -0,0 +1,11 @@
+import { Section } from "../styles/Section"
+
+const CompletePurchase = () => {
+    return (
+        <Section>
+            <h1>complete order page</h1>
+        </Section>
+    )
+}
+
+export default CompletePurchase
