40dc4fc4014db4964361463b70a7b98a9e25e250
hubukala <hubertbukala@icloud.com>
2023-02-26 18:31:40 +0100

    added layout for complete purchase page

--- src/pages/CompletePurchase.js
+++ src/pages/CompletePurchase.js
@@ -1,9 +1,22 @@
+import { useParams } from "react-router-dom"
 import { Section } from "../styles/Section"
+import ConfirmPurchaseForm from "../components/ConfirmPurchaseForm"
+import { CompletePurchaseContainer } from "../styles/complete-purchase/CompletePurchaseContainer"
 
 const CompletePurchase = () => {
+    const productId = useParams()
+    console.log(productId)
+
     return (
         <Section>
-            <h1>complete order page</h1>
+            <CompletePurchaseContainer>
+                <div className="border1">
+                    <ConfirmPurchaseForm />
+                </div>
+                <div className="border1">
+                    place for purchased item details
+                </div>
+            </CompletePurchaseContainer>
         </Section>
     )
 }
