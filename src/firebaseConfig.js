9a4f2e38c1719bb3de331b2a4ab5a50e6a44b157
Hubert Bukala <hubertbukala@icloud.com>
2023-03-06 21:51:39 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAHB6VZErTU-D6y_BCN84oGAjdyCu2NH9s",
-  authDomain: "archives-marketplace.firebaseapp.com",
-  projectId: "archives-marketplace",
-  storageBucket: "archives-marketplace.appspot.com",
-  messagingSenderId: "654454385987",
-  appId: "1:654454385987:web:9709d9e39d60d84e9093f6",
-  measurementId: "G-SXG1EQP25Z"
-};
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app);
+// Please upload your own config
