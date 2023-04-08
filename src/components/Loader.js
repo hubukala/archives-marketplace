c7090d38d81f59d77309afb56166ef5aa48f84aa
hubukala <hubertbukala@icloud.com>
2023-04-08 08:58:15 +0200

    created loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,18 @@
+import { SkChase, SkChaseDot, SkChaseContainer } from "../styles/animations/Chase";
+
+const Loader = () => {
+    return (
+        <SkChaseContainer>
+            <SkChase>
+                <SkChaseDot></SkChaseDot>
+                <SkChaseDot></SkChaseDot>
+                <SkChaseDot></SkChaseDot>
+                <SkChaseDot></SkChaseDot>
+                <SkChaseDot></SkChaseDot>
+                <SkChaseDot></SkChaseDot>
+            </SkChase>
+        </SkChaseContainer>
+    )
+}
+
+export default Loader;
