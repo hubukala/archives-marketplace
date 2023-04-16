8ed9718c5072e863db8ba425f3bfcec05a3d3364
hubukala <hubertbukala@icloud.com>
2023-04-16 12:42:16 +0200

    added functionality to change avatar on current user profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
-import { auth } from "../firebaseConfig";
+import { useEffect, useState, useRef } from "react";
+import { auth, db, storage } from "../firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import AccountDetailsForm from "../components/AccountDetailsForm";
 import AccountMyItems from "../components/AccountMyItems";
 import AccountOrders from "../components/AccountOrders";
@@ -11,12 +13,16 @@ import { ProfileSideBar } from "../styles/profile/ProfileSideBar";
 import { ProfileMain } from "../styles/profile/ProfileMain";
 import { MainContainer } from "../styles/profile/MainContainer";
 import { AvatarStyling } from "../styles/profile/AvatarStyling";
-import Avatar from "../assets/images/168732.png"
+import { FileUploadInput } from "../styles/FileUpload";
 import Loader from "../components/Loader";
+import { uuidv4 } from "@firebase/util";
 
 const ProfilePage = () => {
+    const uniqueId = uuidv4()
     const [signedInUser, setSignedInUser] = useState("")
     const [displayMain, setDisplayMain] = useState("account")
+    const [avatar, setAvatar] = useState({avatar: ""})
+    const fileInputRef = useRef(null)
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -25,6 +31,18 @@ const ProfilePage = () => {
             }
         })
     })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const userId = auth.currentUser.uid
+            const docRef = doc(db, "users", userId)
+            const docSnap = await getDoc(docRef);
+            setAvatar({
+                avatar: docSnap.data().avatar,
+            })
+        }
+        fetchData()
+    },[avatar])
 
     const changeMain = (btn) => {
         setDisplayMain(btn)
@@ -40,12 +58,33 @@ const ProfilePage = () => {
         }
     }
 
+    const handleAvatarClick = () => {
+        fileInputRef.current.click()
+    }
+
+    const handleAvatarUpload = (event) => {
+        const file = event.target.files[0]
+        const userId = auth.currentUser.uid
+        const docRef = doc(db, "users", userId)
+        const imageRef = ref(storage, `avatars/${event.target.files[0].name + uniqueId}`)
+        // todo: add delete prevoius avatar from firebase storage
+        uploadBytes(imageRef, file).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+                const data = {avatar: url}
+                setAvatar({avatar: url})
+                updateDoc(docRef, data)
+            })
+
+        })
+    }
+
     return (
         <>        
             {!signedInUser ? <Loader/> : 
             <ProfileContainer>
                 <ProfileHead>
-                    <AvatarStyling src={Avatar} alt="AV"/> <br />
+                    <FileUploadInput type="file" id="file" name="file" ref={fileInputRef} onChange={(event) => handleAvatarUpload(event)}/>
+                    <AvatarStyling width={100} height={100} src={avatar.avatar} alt="AV" onClick={handleAvatarClick}/> <br />
                     {signedInUser.email} <br />
                     Joined in: {(signedInUser.metadata.creationTime).slice(12, 17)}
                 </ProfileHead>
