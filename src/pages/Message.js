e4597af27306a79b5d22c75dd7ca672e1854a7fd
hubukala <hubertbukala@icloud.com>
2023-08-05 14:54:24 +0200

    fixed message screen interface

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -4,11 +4,12 @@ import { getDocs, query, where, collection } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 import ConfirmPurchaseCard from '../components/ConfirmPurchaseCard';
 import { MessageContainer } from '../styles/message/MessageContainer';
-import { FormContainer } from '../styles/complete-purchase/FormContainer';
-import {
-    InputTextArea,
-    InputsSection,
-} from '../styles/post-item-form/PostItemFormStyles';
+import { InputTextArea } from '../styles/post-item-form/PostItemFormStyles';
+import { ItemContainer } from '../styles/message/ItemContainer';
+import { InputContainer } from '../styles/message/InputContainer';
+import { HeaderOne } from '../styles/shared/text/HeaderOne';
+import { ButtonPrimary } from '../styles/shared/buttons/ButtonPrimary';
+import { ButtonSecondary } from '../styles/shared/buttons/ButtonSecondary';
 import { useFormik } from 'formik';
 import { InputError } from '../styles/post-item-form/InputError';
 import * as Yup from 'yup';
@@ -54,33 +55,37 @@ const Message = () => {
     });
 
     return (
-        <>
-            <ConfirmPurchaseCard
-                key={data.id}
-                image={data.image}
-                title={data.title}
-                size={data.size}
-                price={data.price}
-                id={data.id}
-                category={data.category}
-            />
-            <MessageContainer>
-                <form onSubmit={formik.handleSubmit}>
-                    <InputsSection>
-                        <h3>MESSAGE</h3>
-                        <InputTextArea
-                            type="text"
-                            name="message"
-                            placeholder="Type in your message"
-                            onChange={formik.handleChange}
-                        />
-                        {formik.touched.message && formik.errors.message ? (
-                            <InputError>{formik.errors.message}</InputError>
-                        ) : null}
-                    </InputsSection>
-                </form>
-            </MessageContainer>
-        </>
+        <MessageContainer>
+            <HeaderOne>MESSAGE</HeaderOne>
+            <ItemContainer>
+                <ConfirmPurchaseCard
+                    key={data.id}
+                    image={data.image}
+                    title={data.title}
+                    size={data.size}
+                    price={data.price}
+                    id={data.id}
+                    category={data.category}
+                />
+            </ItemContainer>
+            <form onSubmit={formik.handleSubmit}>
+                <InputContainer>
+                    <InputTextArea
+                        type="text"
+                        name="message"
+                        placeholder="Type in your message"
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.message && formik.errors.message ? (
+                        <InputError>{formik.errors.message}</InputError>
+                    ) : null}
+                    <div>
+                        <ButtonSecondary>CANCEL</ButtonSecondary>
+                        <ButtonPrimary>SEND</ButtonPrimary>
+                    </div>
+                </InputContainer>
+            </form>
+        </MessageContainer>
     );
 };
 
